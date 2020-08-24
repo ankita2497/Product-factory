66c51c8190a987c7521616b717c86526c92f7c13
ankita2497 <ankitaba140@gmail.com>
2020-08-24 12:19:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let createDate = new Date("sep 8, 2020 12:00:00").getTime();
+let x = setInterval(function () {
+  let now = new Date().getTime();
+  let diff = createDate - now;
+  let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let mins = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  let sec = Math.floor((diff % (1000 * 60)) / 1000);
+  document.querySelector(
+    ".Demo"
+  ).innerHTML = `${days}days : ${hours}hrs : ${mins}mins : ${sec}s`;
+}, 1000);
